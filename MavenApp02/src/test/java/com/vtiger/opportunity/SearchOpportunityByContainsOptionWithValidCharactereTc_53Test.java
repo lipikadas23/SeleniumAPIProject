aeb523d18c610f6d221363e2129d86c0fdd3c2d3
priank <priank@Deepika>
2020-01-06 17:28:18 +0530

    u

--- MavenApp02/src/test/java/com/vtiger/opportunity/SearchOpportunityByContainsOptionWithValidCharactereTc_53Test.java
+++ MavenApp02/src/test/java/com/vtiger/opportunity/SearchOpportunityByContainsOptionWithValidCharactereTc_53Test.java
@@ -100,7 +100,7 @@ public class SearchOpportunityByContainsOptionWithValidCharactereTc_53Test exten
 		
 		
 		Reporter.log("Search button clicked succesfully..");
-		Reporter.log("Search button clicked succesfully..");
+		
 		
 		
 	}
